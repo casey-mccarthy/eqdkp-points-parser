9df3a628c06921427ffb9626729488ad9116eaa9
Casey McCarthy <casey@camccarthy.com>
2025-03-10 21:34:39 +0300

    chore: remove unused create_engine import in core/models.py

--- core/models.py
+++ core/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine, Column, Integer, String, Float, Boolean, ForeignKey, DateTime
+from sqlalchemy import Column, Integer, String, Float, Boolean, ForeignKey, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, backref
 from datetime import datetime
